553839b9cd221140a0e7c6911483ad0de51dea3a
Alexander Sidorkin <sashel007@yandex.ru>
2023-09-05 12:47:39 +0300

    кнопка плавает по экрану поверх приложений

--- app/src/main/java/ru/ikar/floatingbutton_ikar/FloatingButtonService.kt
+++ app/src/main/java/ru/ikar/floatingbutton_ikar/FloatingButtonService.kt
@@ -1,9 +1,11 @@
 package ru.ikar.floatingbutton_ikar
 
+import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
 import android.widget.FrameLayout
@@ -11,9 +13,16 @@ import android.widget.FrameLayout
 class FloatingButtonService() : Service() {
     private lateinit var windowManager: WindowManager
     private lateinit var floatingButtonLayout: View
+    private var initialX: Int = 0
+    private var initialY: Int = 0
+    private var initialTouchX: Float = 0f
+    private var initialTouchY: Float = 0f
+    private var lastAction: Int? = null
+
 
     override fun onBind(intent: Intent?): IBinder? = null
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate() {
         super.onCreate()
 
@@ -35,6 +44,41 @@ class FloatingButtonService() : Service() {
         )
 
         windowManager.addView(floatingButtonLayout, params)
+
+        floatingButtonLayout.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // Initial position
+                    initialX = params.x
+                    initialY = params.y
+
+                    // Touch point
+                    initialTouchX = event.rawX
+                    initialTouchY = event.rawY
+
+                    lastAction = event.action
+                    true
+                }
+                MotionEvent.ACTION_UP -> {
+                    lastAction = event.action
+                    true
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    // Calculate the X and Y coordinates of the view.
+                    params.x = initialX + (event.rawX - initialTouchX).toInt()
+                    params.y = initialY + (event.rawY - initialTouchY).toInt()
+
+                    // Update the layout with new X & Y coordinates
+                    windowManager.updateViewLayout(floatingButtonLayout, params)
+                    lastAction = event.action
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+
     }
 
     override fun onDestroy() {
